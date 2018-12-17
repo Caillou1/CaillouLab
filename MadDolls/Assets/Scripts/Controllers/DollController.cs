d4d0f2778a5875ac78199ee1f90992d1e3699f80
Unknown <p.chapelet@rubika-edu.com>
2018-12-17 13:29:49 +0100

    maddolls wip controller

---
+++ MadDolls/Assets/Scripts/Controllers/DollController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DollController : MonoBehaviour
+{
+    public float MovementSpeed = 10f;
+
+    private Rigidbody rigidBody;
+    private Transform tf;
+    private Vector3 inputDirection;
+
+    void Start()
+    {
+        tf = transform;
+        rigidBody = GetComponent<Rigidbody>();
+    }
+    
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        inputDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        rigidBody.MovePosition(tf.position + inputDirection * Time.deltaTime * MovementSpeed);
+    }
+}
---
+++ MadDolls/Assets/Scripts/Controllers/KeepCharacterUpright.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeepCharacterUpright : MonoBehaviour
+{
+    public bool KeepUpRight = true;
+    public float UpForce = 10f;
+    public float UpOffset = 1.45f;
+    public float AdditionalUpForce = 10f;
+    public float DampenAngularForce = 0;
+
+    private Rigidbody rigidBody;
+    private Transform tf;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        tf = transform;
+    }
+
+    void Update()
+    {
+        if (KeepUpRight)
+        {
+            rigidBody.AddForceAtPosition(new Vector3(0, (UpForce + AdditionalUpForce), 0), tf.position + tf.TransformPoint(new Vector3(0, UpOffset, 0)), ForceMode.Force);
+
+            rigidBody.AddForceAtPosition(new Vector3(0, -UpForce, 0), tf.position + tf.TransformPoint(new Vector3(0, -UpOffset, 0)), ForceMode.Force);
+        }
+
+        if(DampenAngularForce > 0)
+        {
+            rigidBody.angularVelocity *= (1 - Time.deltaTime * DampenAngularForce);
+        }
+    }
+}
