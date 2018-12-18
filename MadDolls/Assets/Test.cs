f4ae83c76e4641a55a7117d5324460a51b73efdb
Unknown <p.chapelet@rubika-edu.com>
2018-12-18 10:05:56 +0100

    Final IK

---
+++ MadDolls/Assets/Test.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public bool DisableGravity = true;
+
+    void Start()
+    {
+        var rbs = GetComponentsInChildren<Rigidbody>();
+        Process(rbs);
+    }
+
+    void Process(Rigidbody[] rbs)
+    {
+        foreach(var rb in rbs)
+        {
+            rb.useGravity = false;
+            rb.isKinematic = true;
+            var newRbs = rb.GetComponentsInChildren<Rigidbody>();
+            Process(newRbs);
+        }
+    }
+}
