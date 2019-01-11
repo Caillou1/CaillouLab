2019efe05749d58d30d2ff22403ccfd0a7a245e0
Unknown <p.chapelet@rubika-edu.com>
2019-01-11 20:37:37 +0100

    Tests on Aim IK (gun gameplay soon)

--- MadDolls/Assets/_Scripts/_Character/Components/CharacterControllerComponent.cs
+++ MadDolls/Assets/_Scripts/_Character/Components/CharacterControllerComponent.cs
@@ -1,11 +1,23 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class CharacterControllerComponent : CharacterComponent
 {
+    public bool UseKeyboardMouse = true;
     public bool DebugDirections = true;
     public Vector3 LeftStickDirection { get; private set; }
     public Vector3 RightStickDirection { get; private set; }
 
+    private Action getRightStickAction;
+    private Camera mainCamera;
+
+    private void Awake() {
+        Action keyboardAction = SetRightStickFromKeyboard;
+        Action gamepadAction = SetRightStickFromGamepad;
+        getRightStickAction = UseKeyboardMouse ? keyboardAction : gamepadAction;
+
+        mainCamera = Camera.main;
+    }
     private void Update()
     {
         ManageInput();
@@ -29,6 +41,17 @@ public class CharacterControllerComponent : CharacterComponent
     private void UpdateInputDirections()
     {
         LeftStickDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+        getRightStickAction.Invoke();
+    }
+
+    private void SetRightStickFromGamepad() {
         RightStickDirection = new Vector3(Input.GetAxis("Mouse X"), 0, Input.GetAxis("Mouse Y")).normalized;
+    }
+
+    private void SetRightStickFromKeyboard() {
+        Vector2 controlledCharacterPositionOnScreen = mainCamera.WorldToScreenPoint(controlledCharacter.characterTransform.position);
+        Vector2 mousePosition = Input.mousePosition;
+        Vector2 dir = (mousePosition - controlledCharacterPositionOnScreen).normalized;
+        RightStickDirection = new Vector3(dir.x, 0, dir.y);
     }
 }
