c8b6f1efdd19a1131706ef356f2dc82e71a855bf
Caillou <pvc911@hotmail.fr>
2019-03-31 17:56:57 +0200

    work new pc

---
+++ MadDolls/Assets/_Scripts/FPSLimit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSLimit : MonoBehaviour
+{
+    public bool ShowFPS = true;
+    public int FrameLimit = 60;
+
+    private void Awake() {
+        //Application.targetFrameRate = FrameLimit;
+    }
+
+    private void Start() {
+        KU.StartTimer(() => LogFPS(.1f), .1f, true);
+    }
+
+    private void LogFPS(float duration) {
+        float fps = 1.0f/Time.deltaTime;
+        KU.Log(fps, duration, Color.red, true, false);
+    }
+}
