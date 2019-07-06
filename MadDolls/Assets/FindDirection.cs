abab20887112b1278a1654da0173d6ed8adc3c63
Caillou <pvc911@hotmail.fr>
2019-07-06 09:06:48 +0200

    Character Setup

---
+++ MadDolls/Assets/FindDirection.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindDirection : MonoBehaviour
+{
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+       transform.rotation = Quaternion.Euler(new Vector3(0,0,1)); 
+    }
+}
